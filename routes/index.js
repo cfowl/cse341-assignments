8b0f14ecf104c8a9d8ef355f8f2b7ea1b5aa8538
cfowl <goose_7@msn.com>
2022-04-27 21:55:58 -0600

    added links to home route

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,12 @@
 const router = require('express').Router();
 
-router.get('/', (req, res) => res.send('William Fowler'));
+router.get('/', (req, res) => {
+    let html = 'William Fowler';
+    html += '<ul>';
+    html += `<li><a href="/contacts">Contacts</a></li>`;
+    html += '</ul>';
+    res.send(html);
+});
 router.use('/contacts', require('./contacts'));
 
 module.exports = router;
