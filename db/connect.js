27c8bc8136cd404e710b2d2e115aff288420009f
cfowl <goose_7@msn.com>
2022-04-27 21:32:49 -0600

    moved hard coded uri to .env

--- db/connect.js
+++ db/connect.js
@@ -11,7 +11,7 @@ function initDb(callback) {
         return callback(null, _database);
     }
 
-    const client = new MongoClient('mongodb+srv://admin:A6hITfpRxRyjTOZy@cluster0.uasjs.mongodb.net/cse341?retryWrites=true&w=majority');
+    const client = new MongoClient(process.env.MONGO_DB);
     
     client.connect()
     .then(client => {
