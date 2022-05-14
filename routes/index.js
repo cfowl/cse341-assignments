6a2ddf292544520a0a4fb6382e60f5f3a02cbca3
cfowl <goose_7@msn.com>
2022-05-14 12:27:45 -0600

    removed home route

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,5 @@
 const router = require('express').Router();
 
-// home route
-router.get('/', (req, res) => {
-    let html = 'William Fowler';
-    html += '<ul>';
-    html += `<li><a href="/contacts">Contacts</a></li>`;
-    html += `<li><a href="/api-docs">API Docs</a></li>`;
-    html += '</ul>';
-    res.send(html);
-});
-
 // API docs route
 router.use('/', require('./swagger'));
 
