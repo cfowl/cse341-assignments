ceacb45298f1f0755d11f45c3b0f0a54535730cf
cfowl <goose_7@msn.com>
2022-05-14 11:42:44 -0600

    rolling back to May 7

--- server.js
+++ server.js
@@ -1,24 +1,18 @@
 const bodyParser = require('body-parser');
 const mongodb = require('./db/connect');
 const express = require('express');
-const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('./swagger.json');
+
 const port = process.env.PORT || 3000;
 const app = express();
 
-
-app.use(bodyParser.json())
-   .use((req, res, next) => {
-       res.setHeader('Access-Control-Allow-Origin', '*');
-       res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Z-Key');
-       res.setHeader('Content-Type', 'applicatio/json');
-       res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-       next();
+app
+    .use(bodyParser.json())
+    .use((req, res, next) => {
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        next();
     })
-   .use('/', require('./routes'))
-   .use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+    .use('/', require('./routes'));
 
-// connect to MongoDB and start the app
 mongodb.initDb(err => {
     if (err) console.error(err);
     else {
