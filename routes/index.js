2352fc1c4500dc31c1b75dc2afa2701a62f14bf7
cfowl <goose_7@msn.com>
2022-05-14 11:57:05 -0600

    changed routes organization

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,17 @@
 const router = require('express').Router();
 
-// home route
-router.get('/', (req, res) => {
-    let html = 'William Fowler';
-    html += '<ul>';
-    html += `<li><a href="/contacts">Contacts</a></li>`;
-    html += `<li><a href="/api-docs">API Docs</a></li>`;
-    html += '</ul>';
-    res.send(html);
-});
+// // home route
+// router.get('/', (req, res) => {
+//     let html = 'William Fowler';
+//     html += '<ul>';
+//     html += `<li><a href="/contacts">Contacts</a></li>`;
+//     html += `<li><a href="/api-docs">API Docs</a></li>`;
+//     html += '</ul>';
+//     res.send(html);
+// });
+
+// API docs route
+router.use('/', require('./swagger'));
 
 // contacts route
 router.use('/contacts', require('./contacts'));
--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 const bodyParser = require('body-parser');
 const mongodb = require('./db/connect');
 const express = require('express');
-const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('./swagger.json');
 
 const port = process.env.PORT || 3000;
 const app = express();
@@ -12,8 +10,7 @@ app.use(bodyParser.json())
        res.setHeader('Access-Control-Allow-Origin', '*');
        next();
     })
-   .use('/', require('./routes'))
-   .use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+   .use('/', require('./routes'));
 
 // connect to the server and start the app
 mongodb.initDb(err => {
